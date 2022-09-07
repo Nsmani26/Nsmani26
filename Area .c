b20e030932a657949b5328c2c0ca15e9631f10f8
Nsmani26 <112942142+Nsmani26@users.noreply.github.com>
2022-09-07 14:54:19 +0530

    Create Area .c

---
+++ Area .c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+#include<conio.h>
+ main()
+{
+  int  b,h;
+  float area;
+  scanf("%d",&b);
+  scanf("%d",&h);
+  area=0.5×b×h;
+  printf("the area is %f",area);
+  getch();
+   return 0;
+}
